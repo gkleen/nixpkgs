52331a377c76f4b0190d85a22a399da3c94383d9
Gregor Kleen <pngwjpgh@users.noreply.github.com>
2018-11-23 16:35:22 +0100

    Bump gcs to 4.11.1

--- pkgs/games/gcs/default.nix
+++ pkgs/games/gcs/default.nix
@@ -1,37 +1,61 @@
-{ stdenv, fetchFromGitHub, runCommand
-, jdk8, ant
-, jre8, makeWrapper
-, findutils
+{ stdenv, fetchFromGitHub, fetchurl, runCommand
+, jdk10, ant
+, jre10, makeWrapper
+, javaPackages
 }:
 
 let
-  gcs = fetchFromGitHub {
-    owner = "richardwilkes";
-    repo = "gcs";
-    rev = "gcs-4.10.0";
-    sha256 = "1larn85s1imqja0mh3igb5ljk8a8aky1caypm672s1n6ps4pfa6q";
+  versions = {
+    gcs = {
+      repo = "gcs";
+      version = "4.11.1";
+      sha256 = "0as678ig0vsg40zlfcscgb95a8501fa46hd7v3g87chnn8nm7riv";
+    };
+    appleStubs = {
+      repo = "apple_stubs";
+      version = "4.3.0";
+      sha256 = "0m1qw30b19s04hj7nch1mbvv5s698g5dr1d1r7r07ykvk1yh7zsa";
+    };
+    toolkit = {
+      repo = "toolkit";
+      sha256 = "0zcdbnqdrc5753b99gnl102cvb7df6wlpxvgnsys0gkqi6dahq97";
+    };
+    library = {
+      repo = "gcs_library";
+      sha256 = "1y4rsv89mmf5jx43fz7lknjaw7ikydl8ynh2hwfqfwh35i5z706j";
+    };
   };
-  appleStubs = fetchFromGitHub {
+
+  fetchSource = attrs@{ version ? versions.gcs.version, ... }: fetchFromGitHub {
+    inherit (attrs) repo sha256;
     owner = "richardwilkes";
-    repo = "apple_stubs";
-    rev = "gcs-4.3.0";
-    sha256 = "0m1qw30b19s04hj7nch1mbvv5s698g5dr1d1r7r07ykvk1yh7zsa";
+    rev = "gcs-${version}";
   };
-  toolkit = fetchFromGitHub {
-    owner = "richardwilkes";
-    repo = "toolkit";
-    rev = "gcs-4.10.0";
-    sha256 = "0mx5sjwzmpr0vry254l6mz3m7zkkwcac60qaz31cgi3k5pbk04xw";
-  };
-  library = fetchFromGitHub {
-    owner = "richardwilkes";
-    repo = "gcs_library";
-    rev = "gcs-4.10.0";
-    sha256 = "1fgpz0p21f5m28m4kgn9sa82w9d3zxvjx31dk10sqi8hy58ry04p";
+
+  gcs = fetchSource versions.gcs;
+  appleStubs = fetchSource versions.appleStubs;
+  toolkit = fetchSource versions.toolkit;
+  library = fetchSource versions.library;
+
+  trove4j = stdenv.mkDerivation rec {
+    name = "trove4j-${version}";
+    version = "3.0.3";
+
+    src = fetchurl {
+    url = "mirror://maven/net/sf/trove4j/trove4j-${version}.jar";
+      sha512 = "2slfxn88kglxpw9rs2a647mfr7413qvjs7ibvba07fwi8c81y1vp9r9qn8n9sjlkjc74hpn7s1sx0f88482svjjz1ls0g64abyiaf55";
+    };
+
+    phases = "installPhase";
+
+    installPhase = ''
+      mkdir -p $out/share/java
+      cp $src $out/share/java/trove4j-${version}.jar
+    '';
   };
 in stdenv.mkDerivation rec {
   name = "gcs-${version}";
-  version = "4.10.0";
+  inherit (versions.gcs) version;
 
   src = runCommand "${name}-src" { preferLocalBuild = true; } ''
     mkdir -p $out
@@ -43,13 +67,15 @@ in stdenv.mkDerivation rec {
     cp -r ${library} gcs_library
   '';
 
-  buildInputs = [ jdk8 jre8 ant makeWrapper ];
+  buildInputs = [ jdk10 jre10 ant makeWrapper trove4j ];
   buildPhase = ''
+    set -x
+
     cd apple_stubs
     ant
 
     cd ../toolkit
-    ant
+    ant -v -Dbuild.sysclasspath=first
   
     cd ../gcs
     ant
@@ -62,7 +88,7 @@ in stdenv.mkDerivation rec {
 
     find gcs/libraries toolkit/libraries apple_stubs/ \( -name '*.jar' -and -not -name '*-src.jar' \) -exec cp '{}' $out/share/java ';'
     
-    makeWrapper ${jre8}/bin/java $out/bin/gcs \
+    makeWrapper ${jre10}/bin/java $out/bin/gcs \
       --set GCS_LIBRARY ${library} \
       --add-flags "-cp $out/share/java/gcs-${version}.jar com.trollworks.gcs.app.GCS"
   '';  
